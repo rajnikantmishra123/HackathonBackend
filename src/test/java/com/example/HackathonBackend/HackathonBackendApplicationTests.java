415588ce920f8ed2ad15d8f5b834163a49f29a2c
rajnikantmishra123 <rajnikantmishra19122004@gmail.com>
2026-02-26 12:57:15 +0530

    first commit

---
+++ src/test/java/com/example/HackathonBackend/HackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackathonBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
