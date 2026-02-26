b49d935973b14754e0ace8c55830d772c267c055
Shivam Kumar <shivamkumar2687@gmail.com>
2026-02-26 14:14:43 +0530

    Auth controller partiall

--- src/main/java/com/example/HackathonBackend/controller/AuthController.java
+++ src/main/java/com/example/HackathonBackend/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.example.HackathonBackend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/auth")
 @RestController
 public class AuthController {
 
     @GetMapping("test")
     public String test(){
         return "String";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto signupRequestDto){
+        return ResponseEntity.ok(authService.signup(signupRequestDto));
     }
 
 }
